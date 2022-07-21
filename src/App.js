2a4653f8de87b5accef592ba91fcc20cfb0c43d4
Jonathan Cani Kuntz <jckzti@gmail.com>
2022-07-21 12:27:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload TAKANI.
+          Edit <code>src/App.js</code> and save to reload TAKANIS.
         </p>
         <a
           className="App-link"
